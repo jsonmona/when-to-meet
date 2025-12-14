9df95ba1e0114308d2a1072052fd636b04444143
jsonmona <jsonmona@outlook.com>
2025-12-14 22:02:22 +0900

    feat(api): Add endpoint for default tag

--- packages/api/src/route/tag/index.ts
+++ packages/api/src/route/tag/index.ts
@@ -1,6 +1,13 @@
 import z from 'zod';
 import { tagString } from '../../types/tag.ts';
 
+// GET /api/tag/default
+// 기본으로 활성화 되어 있어야 할 태그 목록
+export const TagDefaultResponse = z.object({
+  tags: z.array(tagString),
+});
+
+
 // GET /api/tag/search?q=prefix
 // 검색 문자열로 시작하는 태그를 최대 10개 반환
 export const TagSearchResponse = z.object({
