c876845c3becce6b7bcab1717f48cb4dc4afd274
jsonmona <jsonmona@outlook.com>
2025-12-15 07:20:50 +0900

    feat(backend): Serve public file as well

--- packages/back/src/app.ts
+++ packages/back/src/app.ts
@@ -6,6 +6,8 @@ const port = process.env.PORT || 3000;
 const prefix = '/api';
 
 app.use(express.json({ limit: '10mb' }));
+app.use(express.static('public'));
+
 app.use(prefix, rootRouter).listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
