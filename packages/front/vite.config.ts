1455bafccc8ce9b7afc7eba997ce2d322d2627e0
jsonmona <jsonmona@outlook.com>
2025-12-14 14:08:01 +0900

    feat(frontend): Add scaffolding for router

--- packages/front/vite.config.ts
+++ packages/front/vite.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
   ],
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+    },
+  },
   server: {
     proxy: {
       '/api': {
