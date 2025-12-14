dda6aa00024b63afa2e3a987c4c62e26d904d53e
jsonmona <jsonmona@outlook.com>
2025-12-14 22:15:02 +0900

    feat(api): Add endpoint for creating tag

--- packages/api/src/route/tag/index.ts
+++ packages/api/src/route/tag/index.ts
@@ -7,9 +7,14 @@ export const TagDefaultResponse = z.object({
   tags: z.array(tagString),
 });
 
-
 // GET /api/tag/search?q=prefix
 // 검색 문자열로 시작하는 태그를 최대 10개 반환
 export const TagSearchResponse = z.object({
   tags: z.array(tagString),
+});
+
+// POST /api/tag
+export const TagCreateRequest = z.object({
+  name: z.string().max(30),
+  isDefault: z.boolean().default(false),
 });
