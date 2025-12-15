590172e85634f9e34a1ac77d5170bec3cc5854f3
jsonmona <jsonmona@outlook.com>
2025-12-15 16:08:33 +0900

    chore(frontend): Enable eslint

--- packages/front/eslint.config.js
+++ packages/front/eslint.config.js
@@ -1,27 +1,44 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import reactCompiler from 'eslint-plugin-react-compiler';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import mantine from 'eslint-config-mantine';
 import tseslint from 'typescript-eslint';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig([
   ...mantine,
-  globalIgnores(['dist']),
+  globalIgnores(['dist', '**/*.config.*']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      react.configs.flat.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      reactCompiler.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.app.json', './tsconfig.node.json'],
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      // Obsolete since React v17
+      'react/react-in-jsx-scope': 'off',
     },
   },
   eslintConfigPrettier,
