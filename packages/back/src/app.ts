8741b2cf3b8bef69e83c558e1348b97da2236626
jsonmona <jsonmona@outlook.com>
2025-12-15 22:43:49 +0900

    feat(backend): Setup express session

--- packages/back/src/app.ts
+++ packages/back/src/app.ts
@@ -1,5 +1,16 @@
 import express from 'express';
 import { rootRouter } from './route/index.ts';
+import { RedisStore } from 'connect-redis';
+import session from 'express-session';
+import { createClient } from 'redis';
+
+const redisClient = createClient();
+await redisClient.connect();
+
+const redisStore = new RedisStore({
+  client: redisClient,
+  prefix: 'myapp:',
+});
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,6 +18,15 @@ const prefix = '/api';
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.static('public'));
+
+app.use(
+  session({
+    store: redisStore,
+    resave: false,
+    saveUninitialized: false,
+    secret: 'when-to-meet-secret',
+  })
+);
 app.use(prefix, rootRouter);
 
 // React SPA 처리
